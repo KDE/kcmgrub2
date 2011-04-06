ccacea590c3501e0a757d5085455fe78b75960c9
Alberto Mattea <alberto@mattea.info>
2011-04-06 14:33:48 +0200

    Add license headers, change bad variable name

--- kcmgrub2.py
+++ kcmgrub2.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Grub2 Control Module
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyKDE4.kdecore import *
@@ -10,10 +25,10 @@ from PyQt4 import uic
 import os, locale, re
 try:
   import Xlib, Xlib.display
-  x=True
+  xlibAvailable=True
 except:
   print("No python XLib bindings found")
-  x=False
+  xlibAvailable=False
 import pbkdf2
 
 
@@ -728,7 +743,7 @@ class PyKcm(KCModule):
       self.resDiag.show()
   
   def getScreenResolution(self):
-    if x:
+    if xlibAvailable:
       display = Xlib.display.Display()
       root = display.screen().root
       desktop = root.get_geometry()
